a0174fd303670f8479cd374590caaedd65f0e082
shiyanfeng <shiyanfeng@wisquest.com>
2026-09-16 15:41:32 +0800

    design: replace the monogram with a converging plane mark

--- src/components/BrandMark.vue
+++ src/components/BrandMark.vue
@@ -1,9 +1,8 @@
 <template>
   <span class="brand-mark" aria-hidden="true">
     <svg class="brand-mark__glyph" viewBox="0 0 32 32" fill="none" focusable="false">
-      <path fill="currentColor" d="M5 25 13 7h4L9 25H5Z" />
-      <path fill="currentColor" d="m18.4 7 8 18H22l-5.8-13.1L18.4 7Z" />
-      <path class="brand-mark__bridge" fill="currentColor" d="M12 19h6.7l1.7 3.8H10.3L12 19Z" />
+      <path fill="currentColor" d="M5 5h18l4 4v5H13l-4 4H5V5Z" />
+      <path class="brand-mark__facet" fill="currentColor" d="M9 22h12l6-6v7l-4 4H9l-4-4v-1h4Z" />
     </svg>
   </span>
 </template>
